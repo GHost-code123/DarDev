1e1ad8dbcb1e669722534c5f080679a9cc6a6c1c
didi <eljamiyenaa@gmai.com>
2025-01-17 18:00:41 +0100

    stricture de la page services

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
 $
+// abdelkrim part;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+$(document).ready(function () {
+    $('.more-info-btn').click(function (e) {
+        e.preventDefault();
+        var target = $(this).data('target');
+        $(target).toggle();
+        $(this).text($(this).text() === 'More Info' ? 'Less Info' : 'More Info');
+    });
+});
